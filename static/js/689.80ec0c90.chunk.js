52e31ea3737aba1ed7136af97699fecd55929682
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-07-02 19:27:20 +0300

    Updates

--- static/js/689.80ec0c90.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[689],{1689:function(e,t,i){i.r(t),i.d(t,{default:function(){return s}});var o={title:"HomePage_title__UFLA3",text:"HomePage_text__2XixT"},n=i(3329);function s(){return(0,n.jsxs)("div",{className:o.home_page,children:[(0,n.jsx)("h1",{className:o.title,children:"This is the PhoneBook Application "}),(0,n.jsx)("p",{className:o.text,children:" Enjoy it! "})]})}}}]);
-//# sourceMappingURL=689.80ec0c90.chunk.js.map
