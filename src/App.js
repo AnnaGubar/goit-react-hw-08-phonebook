46deea342be1141bbe030d1c0fd89bb1fd6c7a5d
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-07-02 19:38:43 +0300

    adds PrivateRoute, PublicRoute

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Route, Routes } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
+import {PublicRoute} from './components/PublicRoute'
+import {PrivateRoute} from './components/PrivateRoute'
+
 import AppBar from './components/Navigation/AppBar';
 import { authOperations } from './redux/auth';
 
@@ -11,8 +14,6 @@ const HomePage = lazy(() => import('./pages/HomePage'));
 const RegisterPage = lazy(() => import('./pages/RegisterPage'));
 const LoginPage = lazy(() => import('./pages/LoginPage'));
 const PhonebookPage = lazy(() => import('./pages/PhonebookPage'));
-
-
 
 function App() {
   const dispatch = useDispatch();
@@ -27,10 +28,15 @@ function App() {
       <AppBar />
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/" element={<HomePage />} />
           <Route path="/contacts" element={<PhonebookPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/register" element={<RegisterPage />} /> */}
+
+          <Route path="/" element={<PublicRoute><HomePage /></PublicRoute>}/>
+          <Route path="contacts" element={<PrivateRoute redirectTo="/login"><PhonebookPage /></PrivateRoute>}/>
+          <Route path="login" element={<PublicRoute redirectTo="/contacts" restricted><LoginPage /></PublicRoute>}/>
+          <Route path="register" element={<PublicRoute restricted><RegisterPage /></PublicRoute>}/>
         </Routes>
       </Suspense>
     </>
